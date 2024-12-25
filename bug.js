b7d81d5295e61454a17f1f64d74cc67b792a2cc9
BugHunter-X <askzenai@gmail.com>
2024-12-25 11:52:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+<div class="bg-gradient-to-r from-blue-500 to-purple-500 p-4 rounded-lg shadow-lg">
+  <p>This is some text.</p>
+</div>
+```
+This code snippet uses Tailwind CSS classes to style a div element. However, there might be an issue where the gradient doesn't render correctly or appears unexpected if the `from` and `to` colors are too similar or have conflicting properties.
